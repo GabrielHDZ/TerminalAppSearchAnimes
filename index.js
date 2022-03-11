606a9d8fdc255e13851cd1d79635e13774a584a4
GabrielHDZ <Gabrielhdz0102@gmail.com>
2022-03-10 22:11:52 -0600

    actualizando archivos

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const { menu, pause } = require('./helpers/inquirer');
+require('dotenv').config();
+const { menu, pause, leerInput, listadoAnimes } = require('./helpers/inquirer');
+const { Busqueda } = require('./models/modelo');
+
+const busqueda = new Busqueda()
+//console.log(process)
+console.log(process.env)
+//console.log(process.config)
 
 
 const main = async () => {
@@ -6,8 +13,25 @@ const main = async () => {
     do {
         opcion = await menu();
         console.log(opcion);
-        await pause();
-    } while (opcion!==0);
+        switch (opcion) {
+            case '1':
+                //mostrar mensaje
+                const entrada = await leerInput('termino a buscar')
+                console.log(entrada)
+                //buscar los lugares con el termino escrito en la entrada
+                const response = await busqueda.buscador(entrada)
+                //seleccionar el lugar
+                const elegido = await listadoAnimes(response);
+                //mostrar los resultados
+                const datos = await busqueda.buscadorEspecifico(elegido);
+
+                console.log(datos);
+                break;
+            case '2':
+                break;
+        }
+        if (opcion !== '0') await pause();
+    } while (opcion !== '0')
 
 }
-main();
+main(); 
---
+++ models/modelo.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+
+class Busqueda {
+    historial = ['dato1', 'dato2'];
+    constructor() {
+        //leer los datos de la DB archivo JSON
+    }
+    async buscador(tag = 'uniform') {
+        let rServer = []
+        //se reliza la peticion HTTP
+        console.log(tag);
+        try {
+            //peticion http
+            const instancia = axios.create({
+                baseURL: 'https://api.jikan.moe/v4/anime',
+                params: {
+                    'q': tag,
+                    'type': 'tv',
+                    'page': 1,
+                    'limit': process.env.LIMIT_PAGES
+                    //aqui va el acces token
+                }
+            })
+            //respuesta que aloja toda la informacion respecto a la consulta del endpoint
+            rServer = await instancia.get();
+        } catch (error) {
+            console.error(error);
+        }
+        // console.log(rServer)
+        return rServer.data.data.map(anime => ({
+            id: anime.mal_id,
+            title: anime.title
+        }));
+
+    }
+    // El EndPoint no necesita parametros, solamente concatenar el id
+    async buscadorEspecifico(id = '121212') {
+        let rServer = []
+        //se reliza la peticion HTTP
+        try {
+            //peticion http
+            const instancia = axios.create({
+                baseURL: `https://api.jikan.moe/v4/anime/${id}`
+                /* params: {
+                    'q': tag,
+                    'type': 'tv',
+                    'page': 1,
+                    'limit': process.env.LIMIT_PAGES
+                    //aqui va el acces token
+                } */
+            })
+            //respuesta que aloja toda la informacion respecto a la consulta del endpoint
+            const response = await instancia.get();
+            const { mal_id, images, title, title_english, title_japanese, type, episodes, status, aired, synopsis } = response.data.data;
+            return {
+                id: mal_id,
+                portada: images,
+                titulo: title,
+                tituloEn: title_english,
+                tituloJp: title_japanese,
+                tipo: type,
+                episodios: episodes,
+                estado: status,
+                emision: aired,
+                termino: aired,
+                sinopsis: synopsis
+            }
+        } catch (error) {
+            console.error(error);
+        }
+        //console.log(rServer)
+
+    }
+}
+
+module.exports = { Busqueda }
